f7ed417c366b8d2363c20a42d6ff0d41a078c5e0
Simon Busch <simon.busch@live.fr>
2020-11-20 16:07:02 +0100

    add rating

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,21 @@
 class Cocktail < ApplicationRecord
   has_one_attached :photo
-  
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
+  def rating
+    # raise
+    if self.reviews.length == 0
+      return "⭐️⭐️⭐️"
+    else 
+      count = 0;
+      self.reviews.each do |review|
+      count += review.rating
+      end
+      average = (count / self.reviews.length).floor
+      return "⭐️"*average
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   resources :doses, only:[:destroy]
 end
